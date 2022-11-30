b15fda2a9140d813ba216f46786e93a743fcb502
Adrianoespanho <112988600+Adrianoespanho@users.noreply.github.com>
2022-11-29 21:05:02 -0300

    exercicio0506
    exercicio 005 e 006 29/11

---
+++ ex005/app.js
@@ -0,0 +1,12 @@
+function CalculaQuadrado(){
+let n1 =document.getElementById("nota1").value
+let n2 =document.getElementById("nota2").value
+let n3 =document.getElementById("nota3").value
+let n4 =document.getElementById("nota4").value
+let n5 =document.getElementById("nota5").value
+document.write(n1*n1, "<br>")
+document.write(n2*n2, "<br>")
+document.write(n3*n3, "<br>")
+document.write(n4*n4, "<br>")
+document.write(n5*n5, "<br>")
+}
